c9df6120e31960302415a33edfcc6c460db5babc
Dimitar Milev <milev.1996@gmail.com>
2017-02-15 14:55:13 +0200

    stream api used

--- src/main/java/crawler/CategoryCrawler.java
+++ src/main/java/crawler/CategoryCrawler.java
@@ -1,14 +1,18 @@
 package crawler;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import db.RecipePersister;
 import recipe.Recipe;
 
 public class CategoryCrawler implements Runnable {
 	private String category;
+	private RecipesCrawler crawler = new RecipesCrawler();
 
 	public CategoryCrawler(String category) {
 		this.category = category;
@@ -16,18 +20,37 @@ public class CategoryCrawler implements Runnable {
 
 	@Override
 	public void run() {
-		RecipesCrawler crawler = new RecipesCrawler();
 		RecipePersister rp = new RecipePersister("jdbc:mysql://localhost:3306/recipes?useSSL=false", "root", "123456");
+		getAllPagesAtCategory(category).stream()
+									   .map(page -> getAllRecipesAtPage(page))
+									   .map(recipesAtPage -> recipesAtPage.stream()
+											   							  .map(recipeLink -> getSingleRecipe(recipeLink))
+											   							  .collect(Collectors.toList()))
+									   .forEach(listOfRecipes -> rp.persistListOfRecipes(listOfRecipes));
+
+	}
+
+	private List<String> getAllPagesAtCategory(String category) {
 		try {
-			List<String> pages = crawler.getAllPagesAtCategory(category);
-			for (String page : pages) {
-				List<String> recipesAtPage = crawler.getAllRecipesAtPage(page);
-				for (String recipe : recipesAtPage) {
-					rp.persistRecipe(crawler.getSingleRecipe(recipe));
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			return crawler.getAllPagesAtCategory(category);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	private Recipe getSingleRecipe(String recipeLink) {
+		try {
+			return crawler.getSingleRecipe(recipeLink);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	private List<String> getAllRecipesAtPage(String page) {
+		try {
+			return crawler.getAllRecipesAtPage(page);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
 		}
 	}
 
