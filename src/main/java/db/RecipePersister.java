a2fbea09bb45276b308302440b1cb17ad6e51ba2
Dimitar Milev <milev.1996@gmail.com>
2017-02-16 14:05:39 +0200

    recipe persister improved

--- src/main/java/db/RecipePersister.java
+++ src/main/java/db/RecipePersister.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,8 +12,7 @@ import recipe.Product;
 import recipe.Recipe;
 
 public class RecipePersister {
-	private static final String MAPING_INSERT_QUERY = "INSERT INTO recipes_to_products (recipe_id,product_id) "
-			+ "VALUES (?,?)";
+	private static final String MAPING_INSERT_QUERY = "INSERT INTO recipes_to_products (recipe_id,product_id) VALUES (?,?)";
 	private static final String RECIPE_INSERT_QUERY = "INSERT INTO recipes (name,description) VALUES (?,?)";
 	private static final String PRODUCT_INSERT_QUERY = "INSERT INTO products (name) VALUES (?)";
 	private String dbURL;
@@ -28,72 +25,132 @@ public class RecipePersister {
 		this.password = password;
 	}
 
-	public void persistRecipe(Recipe recipe) throws SQLException {
+	public void persistListOfRecipes(List<Recipe> listOfRecipes) throws SQLException {
 		Connection conn = DriverManager.getConnection(dbURL, username, password);
-		PreparedStatement stm = conn.prepareStatement(PRODUCT_INSERT_QUERY);
-		for (Product product : recipe.getProducts()) {
-			persistProduct(product, stm);
+		PreparedStatement productInsertStm = conn.prepareStatement(PRODUCT_INSERT_QUERY);
+		PreparedStatement recipeInsertStm = conn.prepareStatement(RECIPE_INSERT_QUERY);
+		PreparedStatement mapingInsertStm = conn.prepareStatement(MAPING_INSERT_QUERY);
+
+		for (Recipe recipe : listOfRecipes) {
+			addRecipeBatch(recipeInsertStm, recipe);
+			addProductsBatch(productInsertStm, recipe.getProducts());
 		}
-		stm = conn.prepareStatement(RECIPE_INSERT_QUERY);
-		persistRecipeInfo(recipe, stm);
-		stm.close();
-		persistRecipeToMapTable(recipe, conn);
+		
+		recipeInsertStm.executeBatch();	
+		productInsertStm.executeBatch();
+		
+		
+		System.out.println("MAPING METHOD");
+		createMapingBatch(listOfRecipes, conn, mapingInsertStm);
+		System.out.println("END OF MAPING METHOD");
+		mapingInsertStm.executeBatch();
+		mapingInsertStm.close();
+		productInsertStm.close();
+		recipeInsertStm.close();
+		
 		conn.close();
 
 	}
 
-	private void persistRecipeToMapTable(Recipe recipe, Connection conn) throws SQLException {
-		int recipeID = getRecipeID(recipe, conn);
-		List<Integer> productsIDs = getProductsIDs(recipe, conn);
-		PreparedStatement stm = conn.prepareStatement(MAPING_INSERT_QUERY);
-		stm.setInt(1, recipeID);
-		for (Integer productID : productsIDs) {
-			stm.setInt(2, productID);
-			stm.executeUpdate();
+	private void createMapingBatch(List<Recipe> listOfRecipes, Connection conn, PreparedStatement mapingInsertStm)
+			throws SQLException {
+		PreparedStatement selectRecipeStm = conn.prepareStatement("SELECT * FROM recipes WHERE name = ?");
+		PreparedStatement selectProudctsStm = conn.prepareStatement("SELECT * FROM products WHERE name = ?");
+
+		for (Recipe recipe : listOfRecipes) {
+			int recipeID = getRecipeID(recipe, selectRecipeStm);
+			System.out.println("Recipe ID : " + recipeID);
+			List<Integer> productsIDs = getProductsIDs(recipe, selectProudctsStm);
+			for (Integer productID : productsIDs) {
+				mapingInsertStm.setInt(1, recipeID);
+				mapingInsertStm.setInt(2, productID);
+				mapingInsertStm.addBatch();
+			}
+
 		}
-		stm.close();
+		selectRecipeStm.close();
+		selectProudctsStm.close();
 	}
 
-	private List<Integer> getProductsIDs(Recipe recipe, Connection conn) throws SQLException {
+	private void addProductsBatch(PreparedStatement productInsertStm, List<Product> products) throws SQLException {
+		for (Product product : products) {
+			productInsertStm.setString(1, product.getName());
+			productInsertStm.addBatch();
+		}
+	}
+
+	private void addRecipeBatch(PreparedStatement recipeInsertStm, Recipe recipe) throws SQLException {
+		recipeInsertStm.setString(1, recipe.getName());
+		recipeInsertStm.setString(2, recipe.getDescription());
+		recipeInsertStm.addBatch();
+	}
+
+	// public void persistRecipe(Recipe recipe) throws SQLException {
+	// Connection conn = DriverManager.getConnection(dbURL, username, password);
+	// PreparedStatement stm = conn.prepareStatement(PRODUCT_INSERT_QUERY);
+	// for (Product product : recipe.getProducts()) {
+	// persistProduct(product, stm);
+	// }
+	// stm = conn.prepareStatement(RECIPE_INSERT_QUERY);
+	// persistRecipeInfo(recipe, stm);
+	// stm.close();
+	// persistRecipeToMapTable(recipe, conn);
+	// conn.close();
+	//
+	// }
+
+	// private void persistRecipeToMapTable(Recipe recipe, Connection conn)
+	// throws SQLException {
+	// int recipeID = getRecipeID(recipe, conn);
+	// List<Integer> productsIDs = getProductsIDs(recipe, conn);
+	// PreparedStatement stm = conn.prepareStatement(MAPING_INSERT_QUERY);
+	// stm.setInt(1, recipeID);
+	// for (Integer productID : productsIDs) {
+	// stm.setInt(2, productID);
+	// stm.executeUpdate();
+	// }
+	// stm.close();
+	// }
+
+	private List<Integer> getProductsIDs(Recipe recipe, PreparedStatement selectProudctsStm) throws SQLException {
 		List<Integer> productsIDs = new LinkedList<>();
-		PreparedStatement stm = conn.prepareStatement("SELECT * FROM products WHERE name = ? ");
 		ResultSet rs = null;
 		for (Product product : recipe.getProducts()) {
-			stm.setString(1, product.getName());
-			rs = stm.executeQuery();
+			selectProudctsStm.setString(1, product.getName());
+			rs = selectProudctsStm.executeQuery();
 			if (rs.next())
 				productsIDs.add(rs.getInt("product_id"));
 		}
 		rs.close();
-		stm.close();
 		return productsIDs;
 	}
 
-	private int getRecipeID(Recipe recipe, Connection conn) throws SQLException {
+	private int getRecipeID(Recipe recipe, PreparedStatement selectRecipeStm) throws SQLException {
 		int result = 0;
-		Statement stm = conn.createStatement();
-		ResultSet rs = stm.executeQuery("SELECT * FROM recipes WHERE name = '" + recipe.getName() + "'");
+		selectRecipeStm.setString(1, recipe.getName());
+		ResultSet rs = selectRecipeStm.executeQuery();
 		if (rs.next()) {
 			result = rs.getInt("recipe_id");
 		}
 		rs.close();
-		stm.close();
 		return result;
 	}
 
-	private void persistRecipeInfo(Recipe recipe, PreparedStatement stm) throws SQLException {
-		stm.setString(1, recipe.getName());
-		stm.setString(2, recipe.getDescription());
-		stm.executeUpdate();
-	}
-
-	private void persistProduct(Product product, PreparedStatement stm) throws SQLException {
-		stm.setString(1, product.getName());
-		try {
-			stm.executeUpdate();
-		} catch (SQLIntegrityConstraintViolationException e) {
-
-		}
-	}
+	// private void persistRecipeInfo(Recipe recipe, PreparedStatement stm)
+	// throws SQLException {
+	// stm.setString(1, recipe.getName());
+	// stm.setString(2, recipe.getDescription());
+	// stm.executeUpdate();
+	// }
+	//
+	// private void persistProduct(Product product, PreparedStatement stm)
+	// throws SQLException {
+	// stm.setString(1, product.getName());
+	// try {
+	// stm.executeUpdate();
+	// } catch (SQLIntegrityConstraintViolationException e) {
+	//
+	// }
+	// }
 
 }
